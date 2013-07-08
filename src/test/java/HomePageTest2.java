32add1008cba791cf8fce9cc5c4cf3237938d9f4
kteodorczyk <misiowskazzz@gmail.com>
2013-07-08 14:59:30 +0200

    homepage tests

---
+++ src/test/java/HomePageTest2.java
@@ -0,0 +1,254 @@
+import org.junit.Test;
+
+public class HomePageTest2 {
+
+    @Test
+    /**TERMS
+    * Validates a click 'Are you PSYCHIC? JOIN NOW' button.
+    * SCENARIO
+    * User has entered on a homepage. User clicks on 'Are you PSYCHIC? JOIN NOW' button.
+    * EXPECTED
+    * User will be redirected to register psychic page */
+    public void testAreYouPsychicJoin(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'Register to get 3 minutes chat for free' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'Register to get 3 minutes chat for free' button.
+     * EXPECTED
+     * User will be redirected to register user page */
+    public void testRegisterToGet3Minutes(){}
+
+
+    // ########## HEADER ##########
+
+    @Test
+    /**TERMS
+     * Validates a click login button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on login button.
+     * EXPECTED
+     * User will be redirected to login page */
+    public void testLoginButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click SignUp button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on SingUp button.
+     * EXPECTED
+     * User will be redirected to register user page */
+    public void testLoginSingUp(){}
+
+    @Test
+    /**TERMS
+     * Validates a click Shopping cart icon.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on SingUp button.
+     * EXPECTED
+     * User will be redirected to cart page */
+    public void testShoppingCartClick(){}
+
+    @Test
+    /**TERMS
+     * Validates a click VopLogo icon.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on VopLogo icon.
+     * EXPECTED
+     * Homepage will be refreshed */
+    public void testVopLogoClick(){}
+
+    @Test
+    /**TERMS
+     * Validates a click EnglishMenu button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on EnglishMenu icon.
+     * EXPECTED
+     * Homepage will be refreshed in english language  */
+    public void testEnglishMenuClick(){}
+
+    @Test
+    /**TERMS
+     * Validates a click PolishMenu button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on EnglishMenu icon.
+     * EXPECTED
+     * Homepage will be refreshed in polish language  */
+    public void testPolishMenuClick(){}
+
+    @Test
+    /**TERMS
+     * Validates a click PhysicsButton button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on PhysicsButton button.
+     * EXPECTED
+     * User will be redirected to physics page */
+    public void testPsychicsButtonClick() {}
+
+    @Test
+    /**TERMS
+     * Validates a click Marketplace button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on Marketplace button.
+     * EXPECTED
+     * User will be redirected to Marketplace page */
+    public void testMarketplaceButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click blog button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on blog button.
+     * EXPECTED
+     * User will be redirected to blog page */
+    public void testBlogButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'My account' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'My account' button.
+     * EXPECTED
+     * User will be redirected to 'mailbox' page */
+    public void testMyaccountButton(){}
+
+
+    @Test
+    /**TERMS
+     * Validates a click 'confirm' button in search section without input letters.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'confirm' button.
+     * EXPECTED
+     * User will be redirected to physics page */
+    public void testSearchConfirmButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'confirm' button in search section with correct user name for example "psychic7".
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'confirm' button.
+     * EXPECTED
+     * User will be redirected to user called psychic7 page */
+    public void testSearchInputField(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'confirm' button in search section with random letters or wrong user name.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'confirm' button.
+     * EXPECTED
+     * User will be redirected to 'No results found!' page */
+    public void testSearchWrongInputField(){}
+
+    // ########## FOOTER ##########
+
+    @Test
+    /**TERMS
+     * Validates a click help button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on help button.
+     * EXPECTED
+     * User will be redirected to help page */
+    public void testHelpButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'about us' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on about us button.
+     * EXPECTED
+     * User will be redirected to 'about' page */
+    public void testAboutUsButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'How it works' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'How it works' button.
+     * EXPECTED
+     * User will be redirected to 'How it works' page */
+    public void testHowItWorksButton(){}
+
+    @Test
+    /**TERMS
+    * Validates a click 'About Physic Readings' button.
+    * SCENARIO
+    * User has entered on a homepage. User clicks on 'About Physic Readings' button.
+    * EXPECTED
+    * User will be redirected to 'physic reading' page */
+    public void testAboutPsychicReadingsButton(){}
+
+
+    @Test
+    /**TERMS
+     * Validates a click 'contact us' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'contact us' button.
+     * EXPECTED
+     * User will be redirected to 'contact' page */
+    public void testContactUsButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'privacy policy' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'privacy policy' button.
+     * EXPECTED
+     * User will be redirected to 'privacy policy' page */
+    public void testPrivacyPolicyButton(){}
+
+
+    @Test
+    /**TERMS
+     * Validates a click 'terms & conditions' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'terms & conditions' button.
+     * EXPECTED
+     * User will be redirected to 'terms & conditions' page */
+    public void testTermsConditionsButton(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'connect with us by Facebook' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'connect with us by Facebook' button.
+     * EXPECTED
+     * User will be redirected to facebook.com/PsychicMarketplace */
+    public void testConnectWithUsByFB(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'connect with us by Twitter' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'connect with us by Twitter' button.
+     * EXPECTED
+     * User will be redirected to twitter.com/voiceofpsychic */
+    public void testConnectWithUsByTwitter(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'connect with us by Pinterest' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'connect with us by Pinterest' button.
+     * EXPECTED
+     * User will be redirected to pinterest.com/voiceofpsychic/ */
+    public void testConnectWithUsByPinterest(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

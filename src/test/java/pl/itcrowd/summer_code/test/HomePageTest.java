f261b0c70cca3184d149177af06f168fb44f51ac
m4rcin <marcins202@gmail.com>
2013-07-09 17:23:02 +0200

    Added Tests for Homepage

---
+++ src/test/java/pl/itcrowd/summer_code/test/HomePageTest.java
@@ -0,0 +1,59 @@
+package pl.itcrowd.summer_code.test;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Arquillian.class)
+public class HomePageTest {
+
+    @Drone
+    WebDriver browser;
+
+    @FindBy(css = "#wrap")
+    HomePage homePage;
+
+    @Before
+    public void beforeTests(){}
+
+    @Test
+    /**TERMS
+     * Validates a click 'Are you PSYCHIC? JOIN NOW' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'Are you PSYCHIC? JOIN NOW' button.
+     * EXPECTED
+     * User will be redirected to register psychic page */
+    public void testAreYouPsychicJoin()
+    {
+        //given
+        browser.navigate().to("https://itcrowd.pl/vop/");
+        //when
+        homePage.areYouPsychicJoinClick();
+        //then
+        assertTrue(browser.getCurrentUrl().startsWith("https://itcrowd.pl/vop/view/registerPsychic.jsf"));
+    }
+
+    @Test
+    /**TERMS
+     * Validates a click 'Register to get 3 minutes chat for free' button.
+     * SCENARIO
+     * User has entered on a homepage. User clicks on 'Register to get 3 minutes chat for free' button.
+     * EXPECTED
+     * User will be redirected to register user page */
+    public void testRegisterToGet3Minutes()
+    {
+        //given
+        browser.navigate().to("https://itcrowd.pl/vop/");
+        //when
+        homePage.registerToGet3MinutesClick();
+        //then
+        assertEquals("https://itcrowd.pl/vop/register", browser.getCurrentUrl());
+    }
+}
